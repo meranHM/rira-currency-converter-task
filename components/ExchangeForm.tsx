c1a88390d4bce7e07deee0ed9c9526cd3e168a10
meranHM <mehranshahani6@gmail.com>
2025-06-07 13:50:53 +0330

    Update english texts font and size
    Add exchange rate box

--- components/ExchangeForm.tsx
+++ components/ExchangeForm.tsx
@@ -1,7 +1,10 @@
-const ExchangeForm = () => {
+import { ArrowDown } from "lucide-react"
+import Image from "next/image"
+
+export default function ExchangeForm() {
   return (
     <div
-        className="mt-8"
+        className="w-full max-w-md mt-8 space-y-12"
     >
         <form
             className="p-2 flex flex-col items-center space-y-12"
@@ -9,7 +12,7 @@ const ExchangeForm = () => {
             <fieldset
                 className="flex gap-4"
             >
-                <label htmlFor="irr-to-usd">
+                <label htmlFor="irr-to-usd" className="text-nowrap">
                     <input 
                         type="radio" 
                         id="irr-to-usd" 
@@ -19,13 +22,13 @@ const ExchangeForm = () => {
                     />
                     ریال ایران به دلار آمریکا
                 </label>
-                <label htmlFor="usd-to-irr">
+                <label htmlFor="usd-to-irr" className="text-nowrap">
                     <input 
                         type="radio" 
                         id="usd-to-irr" 
                         name="exchange-type" 
                         value="USDtoIRR" 
-                        className="ml-2 text-nowrap"
+                        className="ml-2"
                     />
                     دلار آمریکا به ریال ایران
                 </label>
@@ -37,15 +40,31 @@ const ExchangeForm = () => {
                         type="number" 
                         id="irr-amount" 
                         name="irr-amount" 
-                        placeholder="0" 
-                        className="ml-2 bg-gray-200 px-4 py-2 rounded-xl"
+                        placeholder="مقدار" 
+                        className="ml-2 bg-gray-200 px-4 py-2 rounded-xl font-roboto"
                     />
-                    ریال
+                    IRT&nbsp;
+                    <Image 
+                        src="/icons/iran-flag.svg"
+                        alt="Iran's flag"
+                        width={32}
+                        height={32}
+                        className="inline-block"
+                    />
                 </label>
             </fieldset>
         </form>
+
+        <div
+            className="flex justify-between items-center text-xl min-h-16 px-4 bg-emerald-500 rounded-xl text-lightGray shadow-xs shadow-emerald-500"
+        >
+            <p
+                className="font-bold"
+            >
+                نرخ تبدیل: 83.550
+            </p>
+            <ArrowDown size={48}/>
+        </div>
     </div>
   )
 }
-
-export default ExchangeForm
