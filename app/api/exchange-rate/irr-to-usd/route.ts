8e0710e662900d2e2a4e0ef07ba1cc0201ec5acf
meranHM <mehranshahani6@gmail.com>
2025-06-07 20:57:39 +0330

    Add route APIs for fetching from exchangerate API

---
+++ app/api/exchange-rate/irr-to-usd/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+
+
+export async function GET() {
+    const apiKey = process.env.EXCHANGERATE_API_KEY
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/IRR/USD`
+
+    try {
+        const response = await fetch(url, { next: { revalidate: 86400 } }) //Caching the result to avoid reaching the maximum request limit of free plan
+
+        if (!response.ok) {
+            return new NextResponse("Error fetching exchange rate", { status: response.status })
+        }
+
+        const data = await response.json()
+        return NextResponse.json(data)
+    } catch (error) {
+        console.error(error)
+        return new NextResponse("Internal server error", { status: 500 })
+    }
+}
