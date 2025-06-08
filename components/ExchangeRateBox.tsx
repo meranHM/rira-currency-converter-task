9dd694b1736949ea2c6d3b6d0774b5563f0982a5
meranHM <mehranshahani6@gmail.com>
2025-06-08 23:40:34 +0330

    Move Exchange Rate to its own component

---
+++ components/ExchangeRateBox.tsx
@@ -0,0 +1,23 @@
+import { ArrowDown } from "lucide-react"
+
+type ExchangeRateBoxProps = {
+    exchangeRate: number | null
+}
+
+
+export default function ExchangeRateBox({ exchangeRate }: ExchangeRateBoxProps) {
+    const roundedExchangeRate = exchangeRate?.toFixed(8)
+
+    return (
+        <div
+            className="flex justify-between items-center text-xl min-h-16 px-4 bg-emerald-500 rounded-xl text-lightGray shadow-xs shadow-emerald-500"
+        >
+            <p
+                className="font-bold"
+            >
+                نرخ تبدیل: {roundedExchangeRate}
+            </p>
+            <ArrowDown size={36}/>
+        </div>
+    )
+}
